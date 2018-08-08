47608080d85594259c981f1db91d47edba457055
Nikhil <nikhilvasdev.enest@gmail.com>
2018-08-08 13:12:09 +0530

    -basic setup

---
+++ src/android/IdScan.java
@@ -0,0 +1,43 @@
+package cordova-plugin-id-scan;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.json.JSONException;
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import java.io.UnsupportedEncodingException;
+import net.idscan.android.dlparser.DLParser;
+import net.idscan.android.pdf417scanner.PDF417ScanActivity;
+
+/**
+ * This class echoes a string called from JavaScript.
+ */
+public class IdScan extends CordovaPlugin {
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
+        if (action.equals("coolMethod")) {
+            String message = args.getString(0);
+            this.coolMethod(message, callbackContext);
+            return true;
+        }
+        return false;
+    }
+
+    private void coolMethod(String message, CallbackContext callbackContext) {
+        if (message != null && message.length() > 0) {
+            callbackContext.success(message);
+        } else {
+            callbackContext.error("Expected one non-empty string argument.");
+        }
+    }
+}
